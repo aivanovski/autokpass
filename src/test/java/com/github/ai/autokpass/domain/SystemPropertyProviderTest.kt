4a45d123266d11379f840f8c41e2f56cbc2edafd
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2022-07-24 09:59:56 +0200

    Add tests for SystemPropertyProvider

---
+++ src/test/java/com/github/ai/autokpass/domain/SystemPropertyProviderTest.kt
@@ -0,0 +1,37 @@
+package com.github.ai.autokpass.domain
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class SystemPropertyProviderTest {
+
+    @Test
+    fun `getSystemProperty should return value`() {
+        // arrange
+        System.setProperty(CUSTOM_PROPERTY_NAME, CUSTOM_PROPERTY_VALUE)
+
+        // act
+        val result = SystemPropertyProvider().getSystemProperty(CUSTOM_PROPERTY_NAME)
+
+        // assert
+        assertThat(result).isEqualTo(CUSTOM_PROPERTY_VALUE)
+    }
+
+    @Test
+    fun `getSystemProperty should return empty value`() {
+        // arrange
+        assertThat(System.getProperty(NON_EXIST_PROPERTY_NAME)).isNull()
+
+        // act
+        val result = SystemPropertyProvider().getSystemProperty(NON_EXIST_PROPERTY_NAME)
+
+        // assert
+        assertThat(result).isEmpty()
+    }
+
+    companion object {
+        private const val CUSTOM_PROPERTY_NAME = "custom_property_name"
+        private const val CUSTOM_PROPERTY_VALUE = "custom_property_value"
+        private const val NON_EXIST_PROPERTY_NAME = "non_exist_property_name"
+    }
+}
